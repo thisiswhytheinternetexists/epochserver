153c99f135c49d629ffcdbf24505095cf8b3eaa1
Felix Mann <djflix@gmail.com>
2016-03-13 11:32:15 +0100

    Initial commit for EpochTime project

---
+++ EpochRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using ServiceStack;
+
+namespace EpochTime
+{
+	[Route("/now")]
+	public class EpochRequest
+	{
+	}
+}
+
---
+++ EpochService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EpochTime
+{
+	public class EpochService : ServiceStack.Service
+	{
+		public long Get(EpochRequest request) {
+			return DateTime.Now.ToUnixTime();
+		}
+	}
+		
+	public static class EpochStatics {
+		public static DateTime FromUnixTime(this long unixTime)
+		{
+			var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+			return epoch.AddSeconds(unixTime);
+		}
+
+		public static long ToUnixTime(this DateTime date)
+		{
+			var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+			return Convert.ToInt64((date - epoch).TotalSeconds);
+		}
+	}
+}
+
